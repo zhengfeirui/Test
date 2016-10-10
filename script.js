791ac78c8efd7900e69899d0d597d58cda17d0de
zhengfeirui <zhengfeirui@gmail.com>
2016-10-10 15:29:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ZhengfeiRui on 10/9/16.
+ */
+
+$.mFancyTitle({
+    mftMissYou: true,
+    mftMissYouTitle: 'Hey! I miss you!', // new document title: it will appear when the user leaves your page and open another tab in the browser
+    mftMissYouFavicon: {
+        'apple-touch-icon-precomposed': 'apple-touch-icon-152x152.jpg',
+        'icon': 'favicon-32x32.png'
+    } ,// new favicons: they will be visible when the user leaves your page and open another tab in the browser
+    // mftTitleBlink: [true, false]
+    mftTitleMarqueeDelay: [500, 1000]
+});
